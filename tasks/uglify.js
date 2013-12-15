3e755a1582840f5444508ed0b761a594bae8143a
jblary <contact@blary.be>
2013-12-15 18:47:36 +0100

    Add banner

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,11 +1,14 @@
 module.exports = function (grunt) {
     
     grunt.config('uglify', {
-	    dist: {
-	        files: {
-	            '<%= distdir %>/widget.min.js': ['<%= builddir %>/src/widget/widget.js'],
-	        }
-	    }
+        dist: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> - Copyright https://github.com/blaryjp/angular-debug-inspector and Angular Batarang authors */'
+            },
+            files: {
+                '<%= distdir %>/widget.min.js': ['<%= builddir %>/src/widget/widget.js'],
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
